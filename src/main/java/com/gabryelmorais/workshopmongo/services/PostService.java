f36a0401ef4cc43477ad4325e31c6128e2718831
Gabryel <gabryel-lourenco@hotmail.com>
2026-03-26 21:54:05 -0300

    Getting Post by Id

---
+++ src/main/java/com/gabryelmorais/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gabryelmorais.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabryelmorais.workshopmongo.domain.Post;
+import com.gabryelmorais.workshopmongo.repository.PostRepository;
+import com.gabryelmorais.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+}
