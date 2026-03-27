73bfd45e50283fde89e464430ed3d632cbab5629
GabryelMorais <gabryel.morais@integradora.com.br>
2026-03-27 14:48:21 -0300

    Query with multiple criteria

--- src/main/java/com/gabryelmorais/workshopmongo/resources/PostResource.java
+++ src/main/java/com/gabryelmorais/workshopmongo/resources/PostResource.java
@@ -1,6 +1,8 @@
 package com.gabryelmorais.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabryelmorais.workshopmongo.domain.Post;
+import com.gabryelmorais.workshopmongo.domain.User;
+import com.gabryelmorais.workshopmongo.dto.UserDTO;
 import com.gabryelmorais.workshopmongo.resources.util.URL;
 import com.gabryelmorais.workshopmongo.services.PostService;
 
@@ -20,6 +24,12 @@ public class PostResource {
 	
 	@Autowired
 	private PostService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findAll(){
+		List<Post> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id){
@@ -31,6 +41,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/gabryelmorais/workshopmongo/services/PostService.java
+++ src/main/java/com/gabryelmorais/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.gabryelmorais.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabryelmorais.workshopmongo.domain.Post;
+import com.gabryelmorais.workshopmongo.domain.User;
 import com.gabryelmorais.workshopmongo.repository.PostRepository;
 import com.gabryelmorais.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,6 +17,10 @@ public class PostService {
 	
 	@Autowired
 	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
 
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
@@ -23,5 +29,10 @@ public class PostService {
 	
 	public List<Post> findByTitle (String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
